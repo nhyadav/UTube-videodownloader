4b20cfae931424ae6be086e6d61dd46a11269944
narayan <42175861+nhyadav@users.noreply.github.com>
2021-12-03 22:30:48 +0530

    done

--- ydfnvr/ydfnvr/settings.py
+++ ydfnvr/ydfnvr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^0j!_nibihado&qs2p9u3qb1(%mi8629d=#m&&@u@5lsck6h*6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['utubvideodownloader.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['utubvideodownloader.herokuapp.com','127.0.0.1']
 
 
 # Application definition
